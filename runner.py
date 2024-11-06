1f2b0710d9a1400f68f33c77f9e1f0912db7f073
Vlad Y <vladjagnion@gmail.com>
2024-11-06 12:31:21 +0000

    Finished draft version of runner.py

--- runner.py
+++ runner.py
@@ -1,4 +1,7 @@
-directions_turns: dict = {"N":["W", "E"], }
+directions_turns: dict = {"N":["W", "E"], "W":["S", "N"], "S":["E", "W"], "E":["N", "S"]}
+forward_directions:dict = {"N":["+", 1], "W":["-", 0], "S":["-", 1], "E":["+", 0]}
+
+"""Make a main function to separate testing from actual functions"""
 
 def create_runner(x: int = 0, y: int = 0, orientation: str = "N"): 
     global runner
@@ -13,10 +16,28 @@ def get_y(runner: dict) -> int:
 def get_orientation(runner: dict) -> str:
     return runner["Direction"]
 
-def turn(runner: dict, direction: str) -> dict:
+def turn(runner: dict, direction: str, direction_turns: dict =directions_turns) -> dict:
+    """direction_turns is a dictionary where current direction is a key, 
+    which gives a list in form [left_direction, right_direction]"""
+    
+    direction = direction.lower()
 
+    if direction == 'left':
+        runner["Direction"] = direction_turns[runner["Direction"]][0] 
+    elif direction == 'right':
+        runner["Direction"] = direction_turns[runner["Direction"]][1]
     return runner
 
+def forward(runner: dict, forward_directions:dict = forward_directions):
+    direction: str = runner["Direction"]
+    operation: list[str,int] = forward_directions[direction]
+    if operation[0] == '+':
+        runner["Position"][operation[1]] += 1
+    elif operation[0] == '-':
+        runner["Position"][operation[1]] -= 1
 
-create_runner(1,3,"W")
-print(get_orientation(runner))
+
+create_runner(1,3,"N")
+turn(runner, "Left")
+forward(runner)
+print(runner)
